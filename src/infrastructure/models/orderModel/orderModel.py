cdf8e43573d6b19008fdf5c463ebe65998d7a794
juancho391 <juanchischis2010@hotmail.com>
2025-10-20 14:20:23 -0500

    model for oder entity

---
+++ src/infrastructure/models/orderModel/orderModel.py
@@ -0,0 +1,13 @@
+from sqlmodel import Field, SQLModel  # pyright: ignore[reportUnknownVariableType]
+from datetime import datetime, timezone
+
+
+class OrderModel(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    id_user: int = Field(foreign_key="usermodel.id", nullable=False)
+    total_price: int = Field(nullable=False)
+    customer_name: str = Field(nullable=False, max_length=100)
+    service_price: int = Field(default=0)
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc), nullable=False
+    )
