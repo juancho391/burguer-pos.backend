3704546d018f187f83a6597f05644cb8be31282f
juancho391 <juanchischis2010@hotmail.com>
2025-10-20 14:20:59 -0500

    model for product entity

---
+++ src/infrastructure/models/productModels/productModel.py
@@ -0,0 +1,8 @@
+from sqlmodel import Field, SQLModel  # pyright: ignore[reportUnknownVariableType]
+
+
+class ProductModel(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str = Field(nullable=False, max_length=100)
+    description: str | None = None
+    price: float
