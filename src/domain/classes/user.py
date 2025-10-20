c5ef02c08c897156dfaeea6d7a5305bf78574886
juancho391 <juanchischis2010@hotmail.com>
2025-10-20 14:42:04 -0500

    entity user for domain layer

---
+++ src/domain/classes/user.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from pydantic import EmailStr
+from typing import Optional, Type
+
+
+@dataclass
+class User:
+    name: str
+    email: EmailStr
+    password: str
+    id: Optional[int] = None
+
+
+@classmethod
+def create_new_one(
+    cls: "Type[User]", name: str, email: EmailStr, password: str
+) -> "User":
+    return cls(name=name, email=email, password=password)
+
+
+@classmethod
+def create_from_db(
+    cls: "Type[User]", id: int, name: str, email: EmailStr, password: str
+) -> "User":
+    return cls(id=id, name=name, email=email, password=password)
