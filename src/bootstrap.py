64454442f982c6580ba609a2e82358ce0a5f538a
juancho391 <juanchischis2010@hotmail.com>
2025-10-20 19:15:30 -0500

    config dependencies injection and basic config for the api like cors and openapi schema

---
+++ src/bootstrap.py
@@ -0,0 +1,14 @@
+from kink import di
+from src.infrastructure.repositories.postgresql_user_repository import (
+    PostgreSqlUserRepository,
+)
+from src.infrastructure.db.db import get_session_database
+from src.domain.repositories.userRepository import IUserRepository
+from src.application.services.userService import UserService
+
+
+def bootstrap_dependencies():
+    # Repositories
+    user_repository = PostgreSqlUserRepository(session=next(get_session_database()))
+    di[IUserRepository] = user_repository
+    di[UserService] = UserService(user_repository=user_repository)
---
+++ src/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from src.infrastructure.db.db import init_db
+from fastapi.middleware.cors import CORSMiddleware
+from src.infrastructure.controllers.userController import router as user_router
+
+app = FastAPI()
+
+init_db()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+from fastapi.openapi.utils import get_openapi
+from typing import Any
+
+app.include_router(user_router)
+
+
+def custom_openapi() -> dict[str, Any]:
+    if app.openapi_schema:
+        return app.openapi_schema  # type: ignore
+
+    openapi_schema = get_openapi(
+        title="Burger POS System Backend",
+        version="1.0.0",
+        description="Backend API for Burger POS System",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+
+    return app.openapi_schema  # type: ignore
+
+
+app.openapi = custom_openapi
