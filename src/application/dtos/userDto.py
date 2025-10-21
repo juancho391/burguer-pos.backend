2a492593bbb800f9815858ea8dbcdc8be15d242d
juancho391 <juanchischis2010@hotmail.com>
2025-10-20 19:43:03 -0500

    validation for min lenght password and name

--- src/application/dtos/userDto.py
+++ src/application/dtos/userDto.py
@@ -1,11 +1,11 @@
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Field  # type: ignore
 from pydantic import EmailStr
 
 
 class CreateUserDto(SQLModel):
-    name: str
+    name: str = Field(min_length=4, max_length=50)
     email: EmailStr
-    password: str
+    password: str = Field(min_length=8, max_length=30)
 
 
 class UserDto(SQLModel):
