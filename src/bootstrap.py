7628be26611ef08cd3726b3f9d3130543b75148a
juancho391 <juanchischis2010@hotmail.com>
2025-10-22 20:32:41 -0500

    register dependency for authservice

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -5,10 +5,19 @@ from src.infrastructure.repositories.postgresql_user_repository import (
 from src.infrastructure.db.db import get_session_database
 from src.domain.repositories.userRepository import IUserRepository
 from src.application.services.userService import UserService
+from src.infrastructure.security.auth import JwtService
 
 
 def bootstrap_dependencies():
     # Repositories
     user_repository = PostgreSqlUserRepository(session=next(get_session_database()))
+
+    # Services
+    jwt_service = JwtService()
+
+    # Dependency Injection
+    di[JwtService] = jwt_service
     di[IUserRepository] = user_repository
-    di[UserService] = UserService(user_repository=user_repository)
+    di[UserService] = UserService(
+        user_repository=user_repository, jwt_service=jwt_service
+    )
