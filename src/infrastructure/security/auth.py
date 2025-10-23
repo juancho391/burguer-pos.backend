4e2ce0d8ba9a2891683a17c1be1ade7d15d0a82b
juancho391 <juanchischis2010@hotmail.com>
2025-10-23 12:59:29 -0500

    verify token method with ouath2_scheme

--- src/infrastructure/security/auth.py
+++ src/infrastructure/security/auth.py
@@ -5,6 +5,11 @@ import jwt
 from datetime import datetime, timedelta, timezone
 
 from dotenv import load_dotenv
+from fastapi.security import OAuth2PasswordBearer
+from src.domain.classes.token import TokenData
+from src.domain.errors.errors import AuthenticationError
+from fastapi import Depends
+from typing import Annotated
 
 load_dotenv()
 
@@ -13,23 +18,33 @@ class JwtService(AuthTokenService):
     JWT_SECRET_KEY: str = os.getenv("SECRET_KEY", "")
     JWT_ALGORITHM = os.getenv("ALGORITHM")
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
     def create_access_token(self, user_id: int, email: str) -> Token:
         expires_at = datetime.now(timezone.utc) + timedelta(
             minutes=int(str(self.JWT_ACCESS_TOKEN_EXPIRE_MINUTES))
         )
-        payload = {"sub": user_id, "email": email, "exp": expires_at}  # type: ignore
+        payload = {"sub": str(user_id), "email": email, "exp": expires_at}  # type: ignore
         token_value = jwt.encode(  # type: ignore
             payload, self.JWT_SECRET_KEY.encode(), algorithm=self.JWT_ALGORITHM  # type: ignore
         )
         return Token(access_token=token_value, token_type="bearer")
 
-    def verify_token(self, token: Token) -> Token:
+    def verify_token(self, token: Token) -> TokenData:
         try:
-            return jwt.decode(  # type: ignore
-                token.access_token, self.JWT_SECRET_KEY, algorithms=[self.JWT_ALGORITHM]  # type: ignore
+            payload = jwt.decode(  # type: ignore
+                token, self.JWT_SECRET_KEY, algorithms=[self.JWT_ALGORITHM]  # type: ignore
             )
+            return TokenData(email=payload.get("email"))
         except jwt.ExpiredSignatureError:
-            raise Exception("Token expired")
+            raise AuthenticationError("Token has expired")
         except jwt.InvalidTokenError:
-            raise Exception("Invalid token")
+            raise AuthenticationError("Invalid Token")
+
+    def get_current_user(
+        self, token: Annotated[Token, Depends(oauth2_scheme)]
+    ) -> TokenData:
+        return self.verify_token(token=token)
+
+
+Current_user = Annotated[TokenData, Depends(JwtService().get_current_user)]
