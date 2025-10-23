8815012baa12a09e6e18cc834103886fcfb7dd74
juancho391 <juanchischis2010@hotmail.com>
2025-10-22 20:34:05 -0500

    class for Token and userInvalidcredentials error

--- src/domain/errors/errors.py
+++ src/domain/errors/errors.py
@@ -9,3 +9,8 @@ class UserAlreadyExistsError(DomainError):
     def __init__(self, email: str):
         self.email = email
         super().__init__(f"A user with the email '{email}' already exists.")
+
+
+class UserInvalidCredentialsError(DomainError):
+    def __init__(self):
+        super().__init__("Invalid email or password.")
