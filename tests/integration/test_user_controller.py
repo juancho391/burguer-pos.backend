fb4423439da170242a611372722d95aa9ab0535f
juancho391 <juanchischis2010@hotmail.com>
2025-10-24 16:42:22 -0500

    auth user test sucess

--- tests/integration/test_user_controller.py
+++ tests/integration/test_user_controller.py
@@ -3,20 +3,30 @@ from tests.boostrap_test import bootstrap_dependencies_test
 from src.main import app
 
 
-bootstrap_dependencies_test()
-client = TestClient(app)
+class TestUserController:
+    bootstrap_dependencies_test()
+    client = TestClient(app)
 
+    def test_create_user_integration_success(self):
 
-def test_create_user_integration_success():
+        payload = {
+            "name": "Jane Doe 2",
+            "email": "janedoe2@gmail.com",
+            "password": "janedoe1234",
+        }
+        response = self.client.post("auth/register/", json=payload)
+        assert response.status_code == 201
+        data = response.json()
+        assert data["name"] == payload["name"]
+        assert data["email"] == payload["email"]
+        assert "id" in data
 
-    payload = {
-        "name": "Jane Doe 2",
-        "email": "janedoe2@gmail.com",
-        "password": "janedoe1234",
-    }
-    response = client.post("auth/register/", json=payload)
-    assert response.status_code == 201
-    data = response.json()
-    assert data["name"] == payload["name"]
-    assert data["email"] == payload["email"]
-    assert "id" in data
+    def test_authenticate_user_integration_success(self):
+        self.test_create_user_integration_success()
+        login_payload = {
+            "email": "janedoe2@gmail.com",
+            "password": "janedoe1234",
+        }
+        response = self.client.post("auth/token/", json=login_payload)
+        assert response.status_code == 200
+        assert "token" in response.json()
