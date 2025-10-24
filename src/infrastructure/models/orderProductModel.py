737cc89208a0240722da610782b00740e4b9719d
juancho391 <juanchischis2010@hotmail.com>
2025-10-24 18:23:47 -0500

    install psyopg2-binary and i change relationshipmodels names

--- src/infrastructure/models/orderProductModel.py
+++ src/infrastructure/models/orderProductModel.py
@@ -4,7 +4,7 @@ from sqlmodel import SQLModel, Field  # pyright: ignore[reportUnknownVariableTyp
 class OrderProductModel(SQLModel, table=True):
     __tablename__ = "OrderProducts"  # type: ignore
     id: int = Field(default=None, primary_key=True)
-    id_order: int = Field(foreign_key="ordermodel.id", nullable=False)
-    id_product: int = Field(foreign_key="productmodel.id", nullable=False)
+    id_order: int = Field(foreign_key="Orders.id", nullable=False)
+    id_product: int = Field(foreign_key="Products.id", nullable=False)
     quantity: int = Field(nullable=False)
     unit_price: int = Field(nullable=False)
