15e31eafe464786127d8f264b8f7457aff90d885
juancho391 <juanchischis2010@hotmail.com>
2025-10-27 19:21:49 -0500

    create a product mehotd in its repository

--- src/infrastructure/repositories/postgresql_product_repository.py
+++ src/infrastructure/repositories/postgresql_product_repository.py
@@ -1,8 +1,10 @@
 from src.domain.repositories.productRepository import IProductRepository
 from sqlmodel import Session
 from src.infrastructure.models.productModel import ProductModel
+from src.infrastructure.models.ingredientModel import IngredientModel
 from src.domain.classes.product import Product
 from src.application.dtos.productDto import CreateProductDto
+from src.infrastructure.models.productIngredientModel import ProductIngredientModel
 
 
 class PostgreSqlProductRepository(IProductRepository):
@@ -10,10 +12,14 @@ class PostgreSqlProductRepository(IProductRepository):
         self.session = session
 
     def create_product(self, product: Product) -> Product:
-        product_model = ProductModel.model_validate(
-            CreateProductDto(**product.__dict__)
-        )
-        self.session.add(product_model)
+        new_product = ProductModel.model_validate(CreateProductDto(**product.__dict__))
+        self.session.add(new_product)
         self.session.commit()
-        self.session.refresh(product_model)
-        return Product.create_from_db(**product_model.model_dump())
+        self.session.refresh(new_product)
+        return Product.create_from_db(
+            id=new_product.id,
+            name=new_product.name,
+            description=new_product.description,
+            price=new_product.price,
+            ingredients=product.ingredients,
+        )
