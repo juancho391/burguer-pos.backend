08b5c4c225801c14ab495f6878b19c308c011e3a
juancho391 <juanchischis2010@hotmail.com>
2025-11-14 08:46:03 -0500

    create a empy order witouth products method

--- src/application/dtos/orderDto.py
+++ src/application/dtos/orderDto.py
@@ -1,26 +1,28 @@
 from sqlmodel import SQLModel
-from src.application.dtos.productDto import ProductOrderDto, ProductDtoResponse
+from src.application.dtos.productDto import ProductDtoResponse
 from datetime import datetime
-from typing import Optional
 
 
+# Dto for validating order creation request
 class CreateOrderDto(SQLModel):
     customer_name: str
-    products: list[ProductOrderDto]
     include_service: bool
 
 
+# Dto for validating order creation in db
 class CreateOrderDbDto(SQLModel):
     id_user: int
     total_price: int
     customer_name: str
     service_price: int
-    created_at: Optional[datetime]
+    created_at: datetime
 
 
+# Dto for validating order response in creating order request
 class OrderDto(CreateOrderDbDto):
     id: int
 
 
+# Dto for validating order response with products
 class OrderWithProductsDto(OrderDto):
     products: list[ProductDtoResponse]
--- src/application/services/orderService.py
+++ src/application/services/orderService.py
@@ -1,5 +1,10 @@
 from kink import inject
-from src.application.dtos.orderDto import CreateOrderDto, OrderWithProductsDto
+from src.application.dtos.orderDto import (
+    CreateOrderDto,
+    OrderWithProductsDto,
+    CreateOrderDbDto,
+    OrderDto,
+)
 from src.domain.classes.order import Order
 from src.domain.repositories.productRepository import IProductRepository
 from src.domain.errors.errors import ProductNotFoundError
@@ -20,51 +25,13 @@ class OrderService:
         self.order_repossitory = order_repossitory
         self.order_product_repository = order_product_repository
 
-    def create_order(self, order: CreateOrderDto) -> OrderWithProductsDto:
-        products_db = []
-        for product in order.products:
-            product_db = self.product_repository.get_product_by_id(product.id)
-            if not product_db:
-                raise ProductNotFoundError(id=product.id)
-            for _ in range(product.quantity):
-                products_db.append(product_db)
-
+    def create_order(self, order: CreateOrderDto) -> OrderDto:
         new_order = Order.create_new_one(
             id_user=1,
             customer_name=order.customer_name,
-            products=products_db,
-            include_service=order.include_service,
         )
-
         order_created = self.order_repossitory.create_order(new_order)
-
-        product_quantities = {}
-        for product in products_db:
-            if product.name in product_quantities:
-                product_quantities[product.name] += 1
-            else:
-                product_quantities[product.name] = 1
-        order_products = []
-        order_prducts_ids = []
-        for product in products_db:
-            if product.id not in order_prducts_ids:
-                order_products.append(
-                    OrderProduct.create_new_one(
-                        quantity=product_quantities[product.name],
-                        unit_price=product.price,
-                        product_id=product.id,
-                        order_id=order_created.id,
-                    )
-                )
-                order_prducts_ids.append(product.id)
-
-        relations_created = self.order_product_repository.create_all_order_product(
-            order_products=order_products
-        )
-        print("order and relations created successfully")
-        if not relations_created:
-            raise Exception("Error creating order products")
-        return OrderWithProductsDto(**order_created.__dict__)
+        return OrderDto(**order_created.__dict__)
 
     def get_all_orders(self):
         orders = self.order_repossitory.get_all_orders()
