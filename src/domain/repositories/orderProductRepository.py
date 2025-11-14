d2b1a80d07b90aba75efea80d64062656d1f4ce3
juancho391 <juanchischis2010@hotmail.com>
2025-11-14 14:57:42 -0500

    change Interface for OrderProductRepo for return a bool

--- src/domain/repositories/orderProductRepository.py
+++ src/domain/repositories/orderProductRepository.py
@@ -6,3 +6,7 @@ class IOrderProductRepository(ABC):
     @abstractmethod
     def create_all_order_product(self, order_products: list[OrderProduct]) -> bool:
         pass
+
+    @abstractmethod
+    def add_product_to_order(self, order_product: OrderProduct) -> bool:
+        pass
