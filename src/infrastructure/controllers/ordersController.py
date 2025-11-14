3ca25d499d7b5bd4888591dde5a16f39e826ae20
juancho391 <juanchischis2010@hotmail.com>
2025-11-14 14:58:16 -0500

    endpoint for add a product to an order

--- src/infrastructure/controllers/ordersController.py
+++ src/infrastructure/controllers/ordersController.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from src.application.dtos.orderDto import CreateOrderDto, OrderWithProductsDto
+from src.application.dtos.productDto import ProductOrderDto
 from src.application.services.orderService import OrderService
 from kink import di
 from fastapi.responses import JSONResponse
 import json
+from src.domain.errors.errors import OrderNotFoundError, ProductNotFoundError
 
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
@@ -17,6 +19,25 @@ def create_order(
     return JSONResponse(
         content=json.loads(order_created.model_dump_json()), status_code=201
     )
+
+
+@router.post("/{order_id}")
+def add_products_to_order(
+    product: ProductOrderDto,
+    order_id: int,
+    service: OrderService = Depends(lambda: di[OrderService]),
+) -> JSONResponse:
+    try:
+        productd_added = service.add_product_to_order(
+            order_id=order_id, product=product
+        )
+        status_request = "Success" if productd_added else "Failed"
+    except OrderNotFoundError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+    except ProductNotFoundError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+
+    return JSONResponse(content=f"Products added {status_request}", status_code=201)
 
 
 @router.get("/", response_model=list[OrderWithProductsDto])
